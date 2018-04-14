bd9d2d2b939669b172ca5c065c2fc131ba9670ec
Chad Chaney <cchane4@gmail.com>
2018-04-14 11:53:38 -0500

    creating index.html and app.js files

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+// importing the Component function from the react dependency
+// creating component with the function we imported from React
+import React, { Component } from "react";
+
+// exporting it as a default class with the name App
+// anything inside this object is called when we import in in other components
+//render() function we are going to return a div with the contents “Hello World”
+export default class App extends Component {
+  render()
+  return (
+    <div>
+    Hello World
+    </div>
+    );
+  }
+}
