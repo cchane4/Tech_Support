2fe9aaad14e61d8766dba40dcfb3cd19a4a4033f
Chad Chaney <cchane4@gmail.com>
2018-04-15 04:13:37 -0500

    creating first component and webpack server

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
-// importing the Component function from the react dependency
-// creating component with the function we imported from React
+/* importing the Component function from the react dependency
+ creating component with the function we imported from React*/
 import React, { Component } from "react";
 
-// exporting it as a default class with the name App
-// anything inside this object is called when we import in in other components
-//render() function we are going to return a div with the contents “Hello World”
+/* exporting it as a default class with the name App
+ anything inside this object is called when we import in in other components
+render() function we are going to return a div with the contents “Hello World”*/
 export default class App extends Component {
-  render()
-  return (
+  render() {
+    return (
     <div>
     Hello World
     </div>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+var webpack = require('webpack');
+/* MODULE.EXPORTS: the entry: object is where we tell webpack to begin bundling. It’s going to read this file, take all of it’s
+dependencies (and all of their dependencies), apply our loaders, then compress them, and output them as a single file.
+ in our output object that file is called  bundle.js  and it’s output path: is our parent directory.
+ LOADERS: we look for ( or test:) any files that end in the js or jsx extension, with the
+ exception of (exclude:) anything in our node_modules folder, and then apply the babel-loader to them. The babel-loader itself then
+ takes some presets and plugins that will make sure we catch all the HTML we’ve written and convert it into JavaScript*/
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties',
+                     'transform-decorators-legacy']
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  }
+};
